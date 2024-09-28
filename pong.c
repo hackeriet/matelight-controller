23c24732b145ba002caca4267a2f540880d145e0
Martin Mikkelsen <mamikk@mamikk.no>
2024-09-28 23:17:53 +0200

    pong: Add widescreen support.

--- pong.c
+++ pong.c
@@ -16,6 +16,8 @@
 #define PADDLE_WIDTH            4
 #define PADDLE_1_Y              1
 #define PADDLE_2_Y              (grid_height - 2)
+#define PADDLE_1_X              1
+#define PADDLE_2_X              (grid_width - 2)
 #define MOVE_NONE               0
 #define MOVE_LEFT               -1
 #define MOVE_RIGHT              1
@@ -35,21 +37,30 @@ static int paddle_2_dir = MOVE_NONE;
 
 static double ball_y = 0.0;
 static double ball_x = 0.0;
-static double ball_dir = DIR_DOWN;
+static double ball_dir = 0.0;
 
 static void setup_game(bool start)
 {
     game_mode = start ? MODE_GAME : MODE_DEAD;
     game_pause = false;
 
-    paddle_1_pos = (grid_width / 2) - (PADDLE_WIDTH / 2);
+    if (grid_widescreen) {
+        paddle_1_pos = (grid_height / 2) - (PADDLE_WIDTH / 2);
+        paddle_2_pos = (grid_height / 2) - (PADDLE_WIDTH / 2);
+    } else {
+        paddle_1_pos = (grid_width / 2) - (PADDLE_WIDTH / 2);
+        paddle_2_pos = (grid_width / 2) - (PADDLE_WIDTH / 2);
+    }
     paddle_1_dir = MOVE_NONE;
-    paddle_2_pos = (grid_width / 2) - (PADDLE_WIDTH / 2);
     paddle_2_dir = MOVE_NONE;
 
     ball_y = (double)(grid_height / 2);
     ball_x = (double)(grid_width / 2);
-    ball_dir = DIR_DOWN;
+    if (grid_widescreen) {
+        ball_dir = DIR_RIGHT;
+    } else {
+        ball_dir = DIR_DOWN;
+    }
 }
 
 static void activate(bool start)
@@ -72,66 +83,130 @@ static bool doit(void)
     /* move 1. paddle */
     paddle_1_pos += paddle_1_dir;
     if (paddle_1_pos < 0) paddle_1_pos = 0;
-    if (paddle_1_pos > (grid_width - PADDLE_WIDTH)) paddle_1_pos = grid_width - PADDLE_WIDTH;
+    if (grid_widescreen) {
+        if (paddle_1_pos > (grid_height - PADDLE_WIDTH)) paddle_1_pos = grid_height - PADDLE_WIDTH;
+    } else {
+        if (paddle_1_pos > (grid_width - PADDLE_WIDTH)) paddle_1_pos = grid_width - PADDLE_WIDTH;
+    }
 
     /* move 2. paddle */
     paddle_2_pos += paddle_2_dir;
     if (paddle_2_pos < 0) paddle_2_pos = 0;
-    if (paddle_2_pos > (grid_width - PADDLE_WIDTH)) paddle_2_pos = grid_width - PADDLE_WIDTH;
+    if (grid_widescreen) {
+        if (paddle_2_pos > (grid_height - PADDLE_WIDTH)) paddle_2_pos = grid_height - PADDLE_WIDTH;
+    } else {
+        if (paddle_2_pos > (grid_width - PADDLE_WIDTH)) paddle_2_pos = grid_width - PADDLE_WIDTH;
+    }
 
     /* move ball */
     ball_x += cos(ball_dir);
     ball_y += sin(ball_dir);
 
     /* check if ball is outside grid */
-    if (ball_y < 0.0 || ball_y >= (double)grid_height) {
-        ball_y = -1.0;
-        ball_x = -1.0;
-        return false;
-    }
-
-    /* check ball/1. paddle collision */
-    if (lround(ball_y) == PADDLE_1_Y && lround(ball_x) >= paddle_1_pos && lround(ball_x) < (paddle_1_pos + PADDLE_WIDTH)) {
-        hit_offset = ((((ball_x - (double)paddle_1_pos) + 0.5) / (double)PADDLE_WIDTH) * 2.0) - 1.0;
-
-        if (hit_offset > 5.0) {
-            ball_dir = DIR_DOWN - ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
-        } else if (hit_offset > 0.0) {
-            ball_dir = DIR_DOWN - ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
-        } else if (hit_offset <= 0.5) {
-            ball_dir = DIR_DOWN + ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
-        } else {
-            ball_dir = DIR_DOWN + ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+    if (grid_widescreen) {
+        if (ball_x < 0.0 || ball_x >= (double)grid_width) {
+            ball_y = -1.0;
+            ball_x = -1.0;
+            return false;
         }
-        ball_y += 1.0;
-    }
-
-    /* check ball/2. paddle collision */
-    if (lround(ball_y) == PADDLE_2_Y && lround(ball_x) >= paddle_2_pos && lround(ball_x) < (paddle_2_pos + PADDLE_WIDTH)) {
-        hit_offset = ((((ball_x - (double)paddle_2_pos) + 0.5) / (double)PADDLE_WIDTH) * 2.0) - 1.0;
-
-        if (hit_offset > 0.5) {
-            ball_dir = DIR_UP + ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
-        } else if (hit_offset > 0.0) {
-            ball_dir = DIR_UP + ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
-        } else if (hit_offset <= 0.5) {
-            ball_dir = DIR_UP - ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
-        } else {
-            ball_dir = DIR_UP - ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+    } else {
+        if (ball_y < 0.0 || ball_y >= (double)grid_height) {
+            ball_y = -1.0;
+            ball_x = -1.0;
+            return false;
         }
-        ball_y -= 1.0;
     }
 
-    /* check ball/left wall collision */
-    if (ball_x <= 0.0) {
-        ball_dir = atan2(sin(ball_dir), cos(ball_dir) * -1.0);
-        ball_x = 0.0;
+    if (grid_widescreen) {
+        /* check ball/1. paddle collision */
+        if (lround(ball_x) == PADDLE_1_X && lround(ball_y) >= paddle_1_pos && lround(ball_y) < (paddle_1_pos + PADDLE_WIDTH)) {
+            hit_offset = ((((ball_y - (double)paddle_1_pos) + 0.5) / (double)PADDLE_WIDTH) * 2.0) - 1.0;
+
+            if (hit_offset > 5.0) {
+                ball_dir = DIR_RIGHT - ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset > 0.0) {
+                ball_dir = DIR_RIGHT - ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset <= 0.5) {
+                ball_dir = DIR_RIGHT + ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else {
+                ball_dir = DIR_RIGHT + ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            }
+            ball_x += 1.0;
+        }
+
+        /* check ball/2. paddle collision */
+        if (lround(ball_x) == PADDLE_2_X && lround(ball_y) >= paddle_2_pos && lround(ball_y) < (paddle_2_pos + PADDLE_WIDTH)) {
+            hit_offset = ((((ball_y - (double)paddle_2_pos) + 0.5) / (double)PADDLE_WIDTH) * 2.0) - 1.0;
+
+            if (hit_offset > 0.5) {
+                ball_dir = DIR_LEFT + ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset > 0.0) {
+                ball_dir = DIR_LEFT + ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset <= 0.5) {
+                ball_dir = DIR_LEFT - ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else {
+                ball_dir = DIR_LEFT - ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            }
+            ball_x -= 1.0;
+        }
+    } else {
+        /* check ball/1. paddle collision */
+        if (lround(ball_y) == PADDLE_1_Y && lround(ball_x) >= paddle_1_pos && lround(ball_x) < (paddle_1_pos + PADDLE_WIDTH)) {
+            hit_offset = ((((ball_x - (double)paddle_1_pos) + 0.5) / (double)PADDLE_WIDTH) * 2.0) - 1.0;
+
+            if (hit_offset > 5.0) {
+                ball_dir = DIR_DOWN - ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset > 0.0) {
+                ball_dir = DIR_DOWN - ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset <= 0.5) {
+                ball_dir = DIR_DOWN + ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else {
+                ball_dir = DIR_DOWN + ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            }
+            ball_y += 1.0;
+        }
+
+        /* check ball/2. paddle collision */
+        if (lround(ball_y) == PADDLE_2_Y && lround(ball_x) >= paddle_2_pos && lround(ball_x) < (paddle_2_pos + PADDLE_WIDTH)) {
+            hit_offset = ((((ball_x - (double)paddle_2_pos) + 0.5) / (double)PADDLE_WIDTH) * 2.0) - 1.0;
+
+            if (hit_offset > 0.5) {
+                ball_dir = DIR_UP + ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset > 0.0) {
+                ball_dir = DIR_UP + ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else if (hit_offset <= 0.5) {
+                ball_dir = DIR_UP - ((M_PI * 0.2) * ((double)rand() / (double)RAND_MAX));
+            } else {
+                ball_dir = DIR_UP - ((M_PI * 0.3) * ((double)rand() / (double)RAND_MAX));
+            }
+            ball_y -= 1.0;
+        }
     }
 
-    /* check ball/right wall collision */
-    if (ball_x >= (double)grid_width) {
-        ball_dir = atan2(sin(ball_dir), cos(ball_dir) * -1.0);
-        ball_x = (double)(grid_width - 1);
+    if (grid_widescreen) {
+        /* check ball/top wall collision */
+        if (ball_y <= 0.0) {
+            ball_dir = atan2(sin(ball_dir) * -1.0, cos(ball_dir));
+            ball_y = 0.0;
+        }
+
+        /* check ball/bottom wall collision */
+        if (ball_y >= (double)grid_height) {
+            ball_dir = atan2(sin(ball_dir) * -1.0, cos(ball_dir));
+            ball_y = (double)(grid_height - 1);
+        }
+    } else {
+        /* check ball/left wall collision */
+        if (ball_x <= 0.0) {
+            ball_dir = atan2(sin(ball_dir), cos(ball_dir) * -1.0);
+            ball_x = 0.0;
+        }
+
+        /* check ball/right wall collision */
+        if (ball_x >= (double)grid_width) {
+            ball_dir = atan2(sin(ball_dir), cos(ball_dir) * -1.0);
+            ball_x = (double)(grid_width - 1);
+        }
     }
 
     return true;
@@ -178,10 +253,10 @@ static void input(int player, int key_idx, bool key_val, int key_state)
                 paddle_idx_2 = PADDLE_1;
             }
 
-            if (key_idx == KEYPAD_LEFT || key_idx == KEYPAD_RIGHT) {
-                if (key_idx == KEYPAD_LEFT && key_val) {
+            if (key_idx == KEYPAD_LEFT || key_idx == KEYPAD_RIGHT || key_idx == KEYPAD_UP || key_idx == KEYPAD_DOWN) {
+                if ((key_idx == KEYPAD_LEFT || key_idx == KEYPAD_UP) && key_val) {
                     move(paddle_idx_1, MOVE_LEFT);
-                } else if (key_idx == KEYPAD_RIGHT && key_val) {
+                } else if ((key_idx == KEYPAD_RIGHT || key_idx == KEYPAD_DOWN) && key_val) {
                     move(paddle_idx_1, MOVE_RIGHT);
                 } else {
                     move(paddle_idx_1, MOVE_NONE);
@@ -203,6 +278,8 @@ static void input(int player, int key_idx, bool key_val, int key_state)
             switch (key_idx) {
                 case KEYPAD_LEFT:
                 case KEYPAD_RIGHT:
+                case KEYPAD_UP:
+                case KEYPAD_DOWN:
                 case KEYPAD_SELECT:
                 case KEYPAD_START:
                 case KEYPAD_B:
@@ -228,14 +305,26 @@ static void draw(char *screen)
         }
     }
 
-    // 1. paddle
-    for (x = 0; x < PADDLE_WIDTH; x++) {
-        set_pixel(screen, PADDLE_1_Y, paddle_1_pos + x, COLOR_WHITE);
-    }
+    if (grid_widescreen) {
+        // 1. paddle
+        for (y = 0; y < PADDLE_WIDTH; y++) {
+            set_pixel(screen, paddle_1_pos + y, PADDLE_1_X, COLOR_WHITE);
+        }
 
-    // 2. paddle
-    for (x = 0; x < PADDLE_WIDTH; x++) {
-        set_pixel(screen, PADDLE_2_Y, paddle_2_pos + x, COLOR_WHITE);
+        // 2. paddle
+        for (y = 0; y < PADDLE_WIDTH; y++) {
+            set_pixel(screen, paddle_2_pos + y, PADDLE_2_X, COLOR_WHITE);
+        }
+    } else {
+        // 1. paddle
+        for (x = 0; x < PADDLE_WIDTH; x++) {
+            set_pixel(screen, PADDLE_1_Y, paddle_1_pos + x, COLOR_WHITE);
+        }
+
+        // 2. paddle
+        for (x = 0; x < PADDLE_WIDTH; x++) {
+            set_pixel(screen, PADDLE_2_Y, paddle_2_pos + x, COLOR_WHITE);
+        }
     }
 
     // ball
